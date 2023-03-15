929b1463d13b9f53e140b62f56b5e5aee207b7e8
Raymond <robertkejn@gmail,com>
2023-03-15 20:22:17 +0400

    Initial commit

---
+++ ht_3_6.1/ht_3_6.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+//#include <algorithm>
+
+int main()
+{
+    std::string str = "Hello world!";
+    std::map<char, int> m1;
+
+    std::cout << "[IN]\n" << str << "\n";
+
+    for (auto i = str.begin(); i != str.end(); i++) {
+        m1[*i]++;
+    }
+    std::sort(m1.begin(), m1.end(), [](std::pair<char,int>  const& a, std::pair<char,int> const& b)->bool {return a.second > b.second;});
+    /*std::vector<std::pair<char, int>> v1;
+    for (auto i = m1.begin(); i != m1.end(); i++) {
+        v1.push_back(*i);
+    }
+
+    std::sort(v1.begin(), v1.end(), [](std::pair<char, int> const& a, std::pair<char, int> const& b)->bool {return a.second > b.second; });
+    std::cout << "[OUT]\n";
+    for (auto i = v1.begin(); i != v1.end(); i++) {
+        std::cout << ((std::pair<char, int>) * i).first << " " << ((std::pair<char, int>) * i).second << "\n";
+    }*/
+
+}
